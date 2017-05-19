38f03735ee896341ba2ef655492d00d581e8a7b0
jinseongho <asdfghjklji@naver.com>
2017-05-19 21:20:30 +0900

    Fixed inputType

--- app/src/main/java/me/ingeni/lottie_for_designer/MainActivity.java
+++ app/src/main/java/me/ingeni/lottie_for_designer/MainActivity.java
@@ -17,7 +17,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
